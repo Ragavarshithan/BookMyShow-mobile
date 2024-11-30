eddcf487cf61cefc0157f6067bf20147ac0a0159
Ragavarshithan <105372507+Ragavarshithan@users.noreply.github.com>
2024-11-30 14:20:48 +0530

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
   @override
 
   Widget build(BuildContext context){
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Text("Ragav")
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Book Tickets",
+              style: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  fontSize: 30
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text("Sri Lanka >",
+              style: TextStyle(
+                fontSize: 15
+              ),
+              ),
+            ),
+          ]
+        ),
+        backgroundColor: Color(0xFF333545),
+        foregroundColor: Colors.white,
       ),
     );
   }
