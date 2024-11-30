05a8bb60949d15b2532d61cc6930e03d0b47562b
Ragavarshithan <105372507+Ragavarshithan@users.noreply.github.com>
2024-11-30 13:03:25 +0530

    Initial commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+
+  Widget build(BuildContext context){
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Text("Ragav")
+        ],
+      ),
+    );
+  }
+  }
